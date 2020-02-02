01326bffa2651bf8f92309f3c3bac8de318bd85a
Willis Aybar <willisaybar@gmail.com>
2020-02-02 04:53:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -26,7 +26,9 @@ def long_planeteer_calls(array)
   end 
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+    array.find do |type|
+    cheese_types.include?(type)
+  end 
 end
