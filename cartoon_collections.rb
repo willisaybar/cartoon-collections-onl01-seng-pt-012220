729078e4e1e540ac6a243bd5832c4df92588de58
Willis Aybar <willisaybar@gmail.com>
2020-02-02 04:27:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,10 +1,12 @@
 def roll_call_dwarves(names)
   names = []
   names.each_with_index do |e|
-    names << i
+    puts names << e
   end
   names
 end
+
+names = ["Doc", "Dopey", "Bashful", "Grumpy"]
 
 def summon_captain_planet# code an argument here
   # Your code here
